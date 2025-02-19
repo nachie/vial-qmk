50eae06b5a0c466230c22b42d4bc9c1631d5b3dd
Nachiket Kulkarni <nachiket_kulkarni@apple.com>
2025-02-19 14:24:01 -0800

    Add roadtrip - needs led number to change to 27 and new led matrix

---
+++ keyboards/nachie/roadtrip/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x22, 0x6F, 0x3E, 0xBC, 0x07, 0x63, 0xC3, 0x15}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
